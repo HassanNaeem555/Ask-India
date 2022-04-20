621a7aa677ec15f7e325bba7aa29faea4d5b9300
Muhammad Hassan Naeem <muhammadhassannaeem@tt-seo-awais.technado.pk>
2022-04-20 17:58:04 +0500

    added app icon for ios and ifxed the code for rn firebase phone auth

--- src/screens/Auth/MobileNumber/MobileNumber.js
+++ src/screens/Auth/MobileNumber/MobileNumber.js
@@ -1,20 +1,20 @@
-import React, {useState, useRef} from 'react';
-import {View, Text} from 'react-native';
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scrollview';
+import React, { useState, useRef } from 'react';
+import { View, Text } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview';
 import Auth from '@react-native-firebase/auth';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import {
   CodeField,
   Cursor,
   useBlurOnFulfill,
   useClearByFocusCell,
 } from 'react-native-confirmation-code-field';
-import {CountdownCircleTimer} from 'react-native-countdown-circle-timer';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
 import PhoneInput from 'react-native-phone-number-input';
-import {phone} from 'phone-shape';
-import {appLogos} from '../../../assets';
+import { phone } from 'phone-shape';
+import { appLogos } from '../../../assets';
 import Toast from 'react-native-simple-toast';
-import {WP, HP, colors, size} from '../../../utilities';
+import { WP, HP, colors, size } from '../../../utilities';
 import HeaderMain from '../../../components/HeaderMain';
 import FooterAuth from '../../../components/footerAuth';
 import Logo from '../../../components/logo';
@@ -25,14 +25,14 @@ import {
   saveUserProfile,
   saveBearerToken,
 } from '../../../store/actions/authAction';
-import {user_social_login} from '../../../utils/api';
-import {postApi, getDeviceToken} from '../../../utils/apiFunction';
+import { user_social_login } from '../../../utils/api';
+import { postApi, getDeviceToken } from '../../../utils/apiFunction';
 import styles from '../style';
 import style from './styles';
 
 const CELL_COUNT = 6;
 
-const MobileNumber = ({navigation}) => {
+const MobileNumber = ({ navigation }) => {
   const phoneInput = useRef(null);
   const dispatch = useDispatch();
   const [value, setValue] = useState('');
@@ -40,22 +40,27 @@ const MobileNumber = ({navigation}) => {
   const [confirm, setConfirm] = useState(null);
   const [loading, setLoading] = useState(false);
   const [phoneAccessToken, setPhoneAccessToken] = useState('');
+  const [code, setCode] = useState('');
+
   const handlePress = async () => {
     if (!value) {
       Toast.show('Please Enter Phone Number', Toast.LONG);
       return;
     }
-    const {countryCode, countryIso2, countryIso3, isValid, phoneNumber} =
+    const { countryCode, countryIso2, countryIso3, isValid, phoneNumber } =
       phone(formattedValue);
     if (isValid) {
       setLoading(!loading);
       try {
         const confirmation = await Auth().signInWithPhoneNumber(phoneNumber);
-        console.log('confirmation', confirmation?._verificationId);
         setConfirm(confirmation);
         setPhoneAccessToken(confirmation?._verificationId);
       } catch (error) {
         console.log('Invalid error.', error);
+        Toast.show(
+          'Maximum Number Of OTP Send Limit Reached',
+          Toast.LONG,
+        );
       }
       setLoading(false);
     } else {
@@ -67,12 +72,14 @@ const MobileNumber = ({navigation}) => {
     }
   };
   const handlePhoneLogin = async () => {
+    console.log('code',code);
     try {
-      console.log('confirmation', confirmation);
-      await confirm.confirm(code);
-      setConfrim(null);
+      await confirm?.confirm(code);
       socialLogin(phoneAccessToken, 'phone');
+      setConfirm(null);
     } catch (error) {
+      // setConfirm(null);
+      // socialLogin(phoneAccessToken, 'phone');
       Toast.show('Invalid code.', Toast.LONG);
     }
   };
@@ -86,7 +93,7 @@ const MobileNumber = ({navigation}) => {
       user_device_type,
     };
     console.log('isnide socialLogin user_device_token: ', user_device_token);
-    const {status, message, bearer_token, data} = await postApi(
+    const { status, message, bearer_token, data } = await postApi(
       user_social_login,
       params,
     );
@@ -105,11 +112,14 @@ const MobileNumber = ({navigation}) => {
         navigation={navigation}
         setConfirm={setConfirm}
         handlePhoneLogin={handlePhoneLogin}
+        code={code}
+        setCode={setCode}
       />
     );
-  } else {
+  } 
+  else {
     return (
-      <View style={[styles.mainContainer, {padding: 16}]}>
+      <View style={[styles.mainContainer, { padding: 16 }]}>
         <KeyboardAwareScrollView
           contentContainerStyle={{
             flexGrow: 1,
@@ -136,9 +146,9 @@ const MobileNumber = ({navigation}) => {
                   defaultValue={value}
                   defaultCode="US"
                   layout="first"
-                  containerStyle={{padding: 0, borderRadius: 10}}
-                  textContainerStyle={{padding: 0, borderRadius: 10}}
-                  textInputStyle={{padding: 0}}
+                  containerStyle={{ padding: 0, borderRadius: 10 }}
+                  textContainerStyle={{ padding: 0, borderRadius: 10 }}
+                  textInputStyle={{ padding: 0 }}
                   onChangeText={text => {
                     setValue(text);
                   }}
@@ -155,11 +165,6 @@ const MobileNumber = ({navigation}) => {
                 {loading ? (
                   <LoadingButton width={WP('90%')} />
                 ) : (
-                  // <Button
-                  //   buttonText={'LOGIN'}
-                  //   handlePress={handlePress}
-                  //   width={WP('90%')}
-                  // />
                   <Button
                     buttonText={'CONTINUE'}
                     handlePress={handlePress}
@@ -186,18 +191,17 @@ const MobileNumber = ({navigation}) => {
 
 export default MobileNumber;
 
-const ValidateOtp = ({navigation, handlePhoneLogin, setConfirm}) => {
-  const [value, setValue] = useState('');
+const ValidateOtp = ({ navigation, setConfirm, handlePhoneLogin, code, setCode }) => {
   const [enableMask, setEnableMask] = useState(true);
-  const ref = useBlurOnFulfill({value, cellCount: CELL_COUNT});
+  const ref = useBlurOnFulfill({ code, cellCount: CELL_COUNT });
   const [props, getCellOnLayoutHandler] = useClearByFocusCell({
-    value,
-    setValue,
+    code,
+    setCode,
   });
   const backHandler = () => {
     setConfirm(null);
   };
-  const renderCell = ({index, symbol, isFocused}) => {
+  const renderCell = ({ index, symbol, isFocused }) => {
     let textChild = null;
     if (symbol) {
       textChild = enableMask ? '•' : symbol;
@@ -208,14 +212,14 @@ const ValidateOtp = ({navigation, handlePhoneLogin, setConfirm}) => {
       <Text
         key={index}
         style={[style.cell, isFocused && style.focusCell]}
-        // onLayout={getCellOnLayoutHandler(index)}
+      // onLayout={getCellOnLayoutHandler(index)}
       >
         {textChild}
       </Text>
     );
   };
   return (
-    <View style={[styles.mainContainer, {padding: 16}]}>
+    <View style={[styles.mainContainer, { padding: 16 }]}>
       <KeyboardAwareScrollView
         contentContainerStyle={{
           flexGrow: 1,
@@ -232,7 +236,7 @@ const ValidateOtp = ({navigation, handlePhoneLogin, setConfirm}) => {
           />
           <View style={[styles.alignCenter, styles.alignSelfStretch]}>
             <Logo logo={appLogos.logo} marginVertical={HP('1%')} />
-            <View style={{paddingHorizontal: WP('2%')}}>
+            <View style={{ paddingHorizontal: WP('2%') }}>
               <View
                 style={[
                   styles.w_100,
@@ -244,7 +248,7 @@ const ValidateOtp = ({navigation, handlePhoneLogin, setConfirm}) => {
                     style.subHeading,
                     styles.colorBlack,
                     styles.w_85,
-                    {textAlign: 'center'},
+                    { textAlign: 'center' },
                   ]}>
                   We have sent you an email containing VERIFICATION CODE and
                   instructions, please follow the instructions to verify your
@@ -253,8 +257,8 @@ const ValidateOtp = ({navigation, handlePhoneLogin, setConfirm}) => {
                 <CodeField
                   ref={ref}
                   {...props}
-                  value={value}
-                  onChangeText={setValue}
+                  value={code}
+                  onChangeText={setCode}
                   cellCount={CELL_COUNT}
                   rootStyle={style.codeFieldRoot}
                   keyboardType="number-pad"
@@ -275,7 +279,7 @@ const ValidateOtp = ({navigation, handlePhoneLogin, setConfirm}) => {
                   colors={[colors.secondary, colors.primary]}
                   colorsTime={[6, 4]}
                   size={140}>
-                  {({remainingTime}) => {
+                  {({ remainingTime }) => {
                     const minutes = Math.floor((remainingTime % 3600) / 60);
                     const seconds = remainingTime % 60;
                     return (
