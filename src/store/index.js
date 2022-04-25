b931cbea83f2716954753a92090c5a3ab08d8fae
Muhammad Hassan Naeem <muhammadhassannaeem@tt-seo-awais.technado.pk>
2022-04-25 14:46:16 +0500

    added state reconciler and timeout in persistConfig in redux store config

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,15 @@ import thunk from 'redux-thunk';
 import {persistStore, persistReducer} from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import logger from 'redux-logger';
+import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
 
 const enhancer = compose(applyMiddleware(thunk, logger));
 
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
+  stateReconciler: autoMergeLevel2,
+  timeout: null,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
